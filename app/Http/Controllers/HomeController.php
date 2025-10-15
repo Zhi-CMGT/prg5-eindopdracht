2b466f966a02b218074a0d92f173ee781cc3c472
Zhi-CMGT <0980617@hr.nl>
2025-10-15 10:16:58 +0200

    database creation route added

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function destinations()
+    {
+
+    }
+
+    public function home()
+    {
+        return view('home');
+    }
+
+    public function attractions()
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('home', [HomeController::class, 'home']);
+Route::get('destinations', [HomeController::class, 'destinations']);
+Route::get('attractions', [HomeController::class, 'attractions']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -17,4 +22,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
